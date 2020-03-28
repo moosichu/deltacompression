162cfd8fb70f933660931d46fd1d2675d089c485
Tom Maenan Read Cutting <readcuttingt@gmail.com>
2020-03-28 10:46:48 +0000

    Cleanup and document code

--- TMRC.DeltaCompression/DeltaCompression.cs
+++ TMRC.DeltaCompression/DeltaCompression.cs
@@ -6,15 +6,59 @@ using Unity.Collections.LowLevel.Unsafe;
 using Unity.Jobs;
 using Unity.Burst;
 using K4os.Compression.LZ4;
+using System;
 
 namespace TMRC.DeltaCompression
 {
 
-    public struct CompressedByte
+    /// <summary>
+    /// An opaque handle that DeltaCompression.DeltaCompress() uses as a data
+    /// allocation destination.
+    ///
+    /// You can initialise it by providing the length of the source data that
+    /// this will store as input. You can call
+    /// DeltaCompression.MaximumDeltaCompressionSize() to workout what the size
+    /// of the array this structure allocates will be.
+    ///
+    /// Once you have run DeltaCompression.DeltaCompress(), you can extract the bytes
+    /// that have been produced by compression by calling GetBytes on the
+    /// CompressedBytesStorage instance.
+    /// </summary>
+    public struct CompressedBytesStorage : IDisposable
     {
-#pragma warning disable CS0649
-        internal byte Data;
-#pragma warning restore CS0649
+        public int Length => Data.Length;
+        internal NativeArray<byte> Data;
+        public void Dispose()
+        {
+            Data.Dispose();
+        }
+
+        public CompressedBytesStorage(int sourceDataLength, Allocator allocator)
+        {
+            Data = new NativeArray<byte>(DeltaCompression.MaximumDeltaCompressionSize(sourceDataLength), allocator);
+        }
+
+        public unsafe NativeSlice<byte> GetBytes()
+        {
+            UnsafeUtility.CopyPtrToStructure(NativeArrayUnsafeUtility.GetUnsafePtr(Data), out int length);
+            return new NativeSlice<byte>(
+                Data,
+                UnsafeUtility.SizeOf<int>(),
+                length
+            );
+        }
+
+        public unsafe int GetNumBytes()
+        {
+            UnsafeUtility.CopyPtrToStructure(NativeArrayUnsafeUtility.GetUnsafePtr(Data), out int length);
+            return length;
+        }
+
+        public unsafe byte* GetBytes(out int length)
+        {
+            UnsafeUtility.CopyPtrToStructure(NativeArrayUnsafeUtility.GetUnsafePtr(Data), out length);
+            return ((byte*)NativeArrayUnsafeUtility.GetUnsafeReadOnlyPtr(Data)) + UnsafeUtility.SizeOf<int>();
+        }
     }
 
     public static class DeltaCompression
@@ -36,7 +80,7 @@ namespace TMRC.DeltaCompression
         private struct EncodeJob : IJob
         {
             [ReadOnly] public NativeSlice<byte> SourceData;
-            public NativeArray<CompressedByte> DestinationData;
+            public NativeArray<byte> DestinationData;
             public LZ4Level CompressionLevel;
 
             public unsafe void Execute()
@@ -72,39 +116,45 @@ namespace TMRC.DeltaCompression
             }
         }
 
-        public unsafe static NativeSlice<byte> GetBytes(this NativeArray<CompressedByte> compressedData)
-        {
-            UnsafeUtility.CopyPtrToStructure(NativeArrayUnsafeUtility.GetUnsafePtr(compressedData), out int length);
-            return new NativeSlice<byte>(
-                compressedData.Reinterpret<byte>(),
-                UnsafeUtility.SizeOf<int>(),
-                length
-            );
-        }
-
-        public unsafe static int GetNumBytes(this NativeArray<CompressedByte> compressedData)
-        {
-            UnsafeUtility.CopyPtrToStructure(NativeArrayUnsafeUtility.GetUnsafePtr(compressedData), out int length);
-            return length;
-        }
-
-        public unsafe static byte* GetBytes(this NativeArray<CompressedByte> compressedData, out int length)
-        {
-            UnsafeUtility.CopyPtrToStructure(NativeArrayUnsafeUtility.GetUnsafePtr(compressedData), out length);
-            return ((byte*)NativeArrayUnsafeUtility.GetUnsafeReadOnlyPtr(compressedData)) + UnsafeUtility.SizeOf<int>();
-        }
-
+        /// <summary>
+        /// Get the size of the NativeArray that will back CompressedBytesStorage
+        /// when you initialise it with the source data length that you wish to
+        /// compress.
+        /// </summary>
         public static int MaximumDeltaCompressionSize(int Length)
         {
             return UnsafeUtility.SizeOf<int>() + LZ4Codec.MaximumOutputSize(Length);
         }
 
+        /// <summary>
+        /// Given two arrays of data (inData0, inOutData1) with minimal
+        /// differences, this function will schedule jobs to compress inOutData1
+        /// based on the differences.
+        ///
+        /// You need to provide a pre-allocated CompressedBytesStorage instance.
+        ///
+        /// IMPORTANT NOTE: inOutData1 is modified in-place during the
+        /// the compression process. So please make a copy of it if you wish
+        /// to preserve it's state pre-compression.
+        ///
+        /// Once these jobs have finished running, you can call GetBytes on
+        /// the CompressedBytesStorage instance to get the compressed data and
+        /// its final size.
+        ///
+        /// inData0 will be needed to reproduce inOutData1 from the compressed
+        /// delta.
+        ///
+        /// Implementation Details:
+        /// We xor the two input arrays (storing the output in the second one)
+        /// in order to generate a byte array of the difference to which we
+        /// then apply LZ4 compression at the specified level.
+        /// </summary>
         public static JobHandle DeltaCompress(
             NativeSlice<byte> inData0,
             NativeSlice<byte> inOutData1, // Will be modified in-place
-            NativeArray<CompressedByte> outCompressedDelta1,
-            LZ4Level compressionLevel,
-            JobHandle jobHandle
+            CompressedBytesStorage outCompressedDelta1,
+            JobHandle jobHandle = default,
+            LZ4Level compressionLevel =  LZ4Level.L00_FAST
         )
         {
             Debug.Assert(inData0.Length == inOutData1.Length);
@@ -118,19 +168,23 @@ namespace TMRC.DeltaCompression
             jobHandle = new EncodeJob()
             {
                 SourceData = inOutData1,
-                DestinationData = outCompressedDelta1,
+                DestinationData = outCompressedDelta1.Data,
                 CompressionLevel = compressionLevel,
             }.Schedule(jobHandle);
 
             return jobHandle;
         }
 
+        /// <summary>
+        /// Given a source array, and the delta-compressed array of the output,
+        /// reconstruct the array that slightly differs from the source array.
+        /// </summary>
         public static JobHandle DeltaDeCompress
         (
             NativeSlice<byte> inData0,
             NativeSlice<byte> inCompressedDelta1,
             NativeSlice<byte> outData1,
-            JobHandle jobHandle
+            JobHandle jobHandle = default
         )
         {
             jobHandle = new DecodeJob()
@@ -145,7 +199,5 @@ namespace TMRC.DeltaCompression
             }.Schedule(inData0.Length, 64, jobHandle);
             return jobHandle;
         }
-
     }
-
 }
